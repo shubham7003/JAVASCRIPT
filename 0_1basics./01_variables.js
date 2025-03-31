5d6d278e99fe43ad9947b6f68576b73181292051
Shubham Kumar Dubey <152261769+shubham7003@users.noreply.github.com>
2025-03-31 13:11:50 +0000

    DAY_1

---
+++ 0_1basics./01_variables.js
@@ -0,0 +1,12 @@
+const accountId =   14456
+let accountEmail = "contakt24hrs@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 0_1basics./02_dataTypes.js
@@ -0,0 +1,24 @@
+// "use strict"; treat all JS code as newer version
+// alert(3+3) we are using nodejs , not browser
+
+// code readiibilty also should be high.
+
+let name = "Hitesh"
+let age = 18
+let isLogin = false
+
+/*
+number
+bigint
+boolean
+string
+null => standalone value
+undefined => not assign any value
+symbol => uniques
+objects
+*/
+
+console.log(typeof "shubham")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
